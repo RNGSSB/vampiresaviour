2273688bad0ae9c14fe3cebdc097f8bfc06dff3f
RNGSSB <59853617+RNGSSB@users.noreply.github.com>
2024-05-10 19:17:13 -0600

    k

---
+++ X/data/common.cmd
@@ -0,0 +1,213 @@
+[Command]
+name = "back"     ; REQUIRED for command buffering
+command = B
+time = 1
+[Command]
+name = "fwd"      ; REQUIRED for command buffering
+command = F
+time = 1
+[Command]
+name = "up"       ; REQUIRED for command buffering
+command = U
+time = 1
+[Command]
+name = "down"     ; REQUIRED for command buffering
+command = D
+time = 1
+
+[Command]
+name = "fwd2"     ; REQUIRED for command buffering
+command = $F
+time = 1
+[Command]
+name = "back2"    ; REQUIRED for command buffering
+command = $B
+time = 1
+[Command]
+name = "up2"
+command = $U      ; REQUIRED for command buffering
+time = 1
+[Command]
+name = "down2"    ; REQUIRED for command buffering
+command = $D
+time = 1
+[Command]
+name = "recovery"
+command = 
+time = 1
+buffer.time = 1
+
+[Command]
+name = "TagShiftBack"
+command = d
+time = 1
+buffer.time = 1
+
+[Command]
+name = "TagShiftFwd"
+command = w
+time = 1
+buffer.time = 1
+
+[Command]
+name = "x"
+command = x
+time = 1
+buffer.time = 1
+
+[Command]
+name = "y"
+command = y
+time = 1
+buffer.time = 1
+
+[Command]
+name = "z"
+command = z
+time = 1
+buffer.time = 1
+
+[Command]
+name = "a"
+command = a
+time = 1
+buffer.time = 1
+
+[Command]
+name = "b"
+command = b
+time = 1
+buffer.time = 1
+
+[Command]
+name = "c"
+command = c
+time = 1
+buffer.time = 1
+
+[Command]
+name = "start"
+command = s
+time = 1
+buffer.time = 1
+
+[Command]
+name = "d"
+command = d
+time = 1
+buffer.time = 1
+
+[Command]
+name = "w"
+command = w
+time = 1
+buffer.time = 1
+
+[Command]
+name = "m"
+command = m
+time = 1
+buffer.time = 1
+
+[Command]
+name = "menu"
+command = m
+time = 1
+buffer.time = 1
+
+[Command]
+name = "holdfwd"
+command = /$F
+time = 1
+buffer.time = 1
+
+[Command]
+name = "holdback"
+command = /$B
+time = 1
+buffer.time = 1
+
+[Command]
+name = "holdup"
+command = /$U
+time = 1
+buffer.time = 1
+
+[Command]
+name = "holddown"
+command = /$D
+time = 1
+buffer.time = 1
+
+[Command]
+name = "hold_x"     ; REQUIRED for command buffering
+command = /x
+time = 1
+[Command]
+name = "hold_y"     ; REQUIRED for command buffering
+command = /y
+time = 1
+[Command]
+name = "hold_z"     ; REQUIRED for command buffering
+command = /z
+time = 1
+[Command]
+name = "hold_w"     ; REQUIRED for command buffering
+command = /w
+time = 1
+[Command]
+name = "hold_a"     ; REQUIRED for command buffering
+command = /a
+time = 1
+[Command]
+name = "hold_b"     ; REQUIRED for command buffering
+command = /b
+time = 1
+[Command]
+name = "hold_c"     ; REQUIRED for command buffering
+command = /c
+time = 1
+[Command]
+name = "hold_d"     ; REQUIRED for command buffering
+command = /d
+time = 1
+[Command]
+name = "hold_start" ; REQUIRED for command buffering
+command = /s
+time = 1
+
+[Command]
+name = "release_fwd"
+command = ~F
+time = 1
+
+[Command]
+name = "release_down"
+command = ~D
+time = 1
+
+[Command]
+name = "release_back"
+command = ~B
+time = 1
+
+[Command]
+name = "release_up"
+command = ~U
+time = 1
+
+[Command]
+name = "release_down"
+command = ~D
+time = 1
+
+[Command]
+name = "release_upfwd"
+command = ~UF
+time = 1
+
+
+[Command]
+name = "release_upback"
+command = ~UB
+time = 1
